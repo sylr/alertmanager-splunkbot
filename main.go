593bf303120605e8d4846427ccae5c42567b9e34
Sylvain Rabot <s.rabot@lectra.com>
2017-12-04 13:18:02 +0100

    alertmanager-splunkbot v0.0.6
    Signed-off-by: Sylvain Rabot <s.rabot@lectra.com>

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ type SplunkbotOptions struct {
 var (
   opts    = SplunkbotOptions{}
   parser  = flags.NewParser(&opts, flags.Default)
-  version = "v0.0.5"
+  version = "v0.0.6"
 )
 
 func init() {
