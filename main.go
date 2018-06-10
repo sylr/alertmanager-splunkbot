12a1c1205f5353679c826e051b8122edf21206be
Sylvain Rabot <sylvain@abstraction.fr>
2018-06-10 17:16:09 +0200

    Apply go fmt
    Signed-off-by: Sylvain Rabot <sylvain@abstraction.fr>

--- main.go
+++ main.go
@@ -1,115 +1,115 @@
-// vim: set tabstop=2 expandtab autoindent smartindent:
+// vim: set tabstop=4 expandtab autoindent smartindent:
 
 package main
 
 import (
-  "os"
-  "fmt"
-  "time"
-  "net/http"
-  "crypto/tls"
-  "github.com/sylr/alertmanager-splunkbot/splunkbot"
-  "github.com/jessevdk/go-flags"
-  log "github.com/sirupsen/logrus"
+	"crypto/tls"
+	"fmt"
+	"github.com/jessevdk/go-flags"
+	log "github.com/sirupsen/logrus"
+	"github.com/sylr/alertmanager-splunkbot/splunkbot"
+	"net/http"
+	"os"
+	"time"
 )
 
 type SplunkbotOptions struct {
-  Verbose           []bool            `short:"v" long:"verbose" description:"Show verbose debug information"`
-  Version             bool            `          long:"version" description:"Show version"`
-  ListeningAddress    string          `short:"a" long:"address" description:"Listening address" env:"SPLUNKBOT_LISTENING_ADDRESS" default:"127.0.0.1"`
-  ListeningPort       uint            `short:"p" long:"port" description:"Listening port" env:"SPLUNKBOT_LISTENING_PORT" default:"44553"`
-  SplunkUrl           string          `short:"u" long:"splunk-url" description:"Splunk HEC endpoint" env:"SPLUNKBOT_SPLUNK_URL" required:"true"`
-  SplunkHTTPTimeout   uint            `short:"n" long:"splunk-timeout" description:"Splunk HEC timeout (seconds)" env:"SPLUNKBOT_SPLUNK_HTTP_TIMEOUT" default:"5"`
-  SplunkToken         string          `short:"t" long:"splunk-token" description:"Splunk HEC token" env:"SPLUNKBOT_SPLUNK_TOKEN" required:"true"`
-  SplunkIndex         string          `short:"i" long:"splunk-index" description:"Splunk index" env:"SPLUNKBOT_SPLUNK_INDEX"`
-  SplunkSourcetype    string          `short:"s" long:"splunk-sourcetype" description:"Splunk event sourcetype" env:"SPLUNKBOT_SPLUNK_SOURCETYPE" required:"true" default:"alertmanager"`
-  SplunkTLSInsecure   bool            `short:"k" long:"insecure" description:"Do not check Splunk TLS certificate"`
+	Verbose           []bool `short:"v" long:"verbose" description:"Show verbose debug information"`
+	Version           bool   `          long:"version" description:"Show version"`
+	ListeningAddress  string `short:"a" long:"address" description:"Listening address" env:"SPLUNKBOT_LISTENING_ADDRESS" default:"127.0.0.1"`
+	ListeningPort     uint   `short:"p" long:"port" description:"Listening port" env:"SPLUNKBOT_LISTENING_PORT" default:"44553"`
+	SplunkUrl         string `short:"u" long:"splunk-url" description:"Splunk HEC endpoint" env:"SPLUNKBOT_SPLUNK_URL" required:"true"`
+	SplunkHTTPTimeout uint   `short:"n" long:"splunk-timeout" description:"Splunk HEC timeout (seconds)" env:"SPLUNKBOT_SPLUNK_HTTP_TIMEOUT" default:"5"`
+	SplunkToken       string `short:"t" long:"splunk-token" description:"Splunk HEC token" env:"SPLUNKBOT_SPLUNK_TOKEN" required:"true"`
+	SplunkIndex       string `short:"i" long:"splunk-index" description:"Splunk index" env:"SPLUNKBOT_SPLUNK_INDEX"`
+	SplunkSourcetype  string `short:"s" long:"splunk-sourcetype" description:"Splunk event sourcetype" env:"SPLUNKBOT_SPLUNK_SOURCETYPE" required:"true" default:"alertmanager"`
+	SplunkTLSInsecure bool   `short:"k" long:"insecure" description:"Do not check Splunk TLS certificate"`
 }
 
 var (
-  opts    = SplunkbotOptions{}
-  parser  = flags.NewParser(&opts, flags.Default)
-  version = "v0.0.9"
+	opts    = SplunkbotOptions{}
+	parser  = flags.NewParser(&opts, flags.Default)
+	version = "v0.0.9"
 )
 
 func init() {
-  // Log as JSON instead of the default ASCII formatter.
-  //log.SetFormatter(&log.JSONFormatter{})
-  log.SetFormatter(&log.TextFormatter{})
+	// Log as JSON instead of the default ASCII formatter.
+	//log.SetFormatter(&log.JSONFormatter{})
+	log.SetFormatter(&log.TextFormatter{})
 
-  // Output to stdout instead of the default stderr
-  // Can be any io.Writer, see below for File example
-  log.SetOutput(os.Stdout)
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
 
-  // Only log the warning severity or above.
-  log.SetLevel(log.DebugLevel)
+	// Only log the warning severity or above.
+	log.SetLevel(log.DebugLevel)
 }
 
 // main
 func main() {
-  // looing for --version in args
-  for _, val := range os.Args {
-    if val == "--version" {
-      fmt.Printf("alertmanager-splunkbot version %s\n", version)
-      os.Exit(0)
-    } else if val == "--" {
-      break
-    }
-  }
+	// looing for --version in args
+	for _, val := range os.Args {
+		if val == "--version" {
+			fmt.Printf("alertmanager-splunkbot version %s\n", version)
+			os.Exit(0)
+		} else if val == "--" {
+			break
+		}
+	}
 
-  if _, err := parser.Parse(); err != nil {
-    if flagsErr, ok := err.(*flags.Error); ok && flagsErr.Type == flags.ErrHelp {
-      os.Exit(0)
-    } else {
-      log.Fatal(err)
-      os.Exit(1)
-    }
-  }
+	if _, err := parser.Parse(); err != nil {
+		if flagsErr, ok := err.(*flags.Error); ok && flagsErr.Type == flags.ErrHelp {
+			os.Exit(0)
+		} else {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+	}
 
-  // Update logging level
-  switch  {
-    case len(opts.Verbose) >= 1:
-      log.SetLevel(log.DebugLevel)
-    default:
-      log.SetLevel(log.InfoLevel)
-  }
+	// Update logging level
+	switch {
+	case len(opts.Verbose) >= 1:
+		log.SetLevel(log.DebugLevel)
+	default:
+		log.SetLevel(log.InfoLevel)
+	}
 
-  // Loggin options
-  log.Debugf("Options: %+v", opts)
+	// Loggin options
+	log.Debugf("Options: %+v", opts)
 
-  // Starting server
-  log.Infof("Version: %s", version)
-  log.Infof("Starting server at http://%s:%v", opts.ListeningAddress, opts.ListeningPort)
+	// Starting server
+	log.Infof("Version: %s", version)
+	log.Infof("Starting server at http://%s:%v", opts.ListeningAddress, opts.ListeningPort)
 
-  // HTTP Transport
-  tr := &http.Transport{
-    TLSClientConfig:  &tls.Config{
-      InsecureSkipVerify: opts.SplunkTLSInsecure,
-    },
-  }
+	// HTTP Transport
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: opts.SplunkTLSInsecure,
+		},
+	}
 
-  // HTTP Client
-  client := &http.Client{
-    Timeout:          time.Duration(time.Duration(opts.SplunkHTTPTimeout) * time.Second),
-    Transport:        tr,
-  }
+	// HTTP Client
+	client := &http.Client{
+		Timeout:   time.Duration(time.Duration(opts.SplunkHTTPTimeout) * time.Second),
+		Transport: tr,
+	}
 
-  // Splunkbot
-  sbot := &splunkbot.Splunkbot{
-    HttpClient:       client,
-    ListeningAddress: opts.ListeningAddress,
-    ListeningPort:    opts.ListeningPort,
-    SplunkSourcetype: opts.SplunkSourcetype,
-    SplunkIndex:      opts.SplunkIndex,
-    SplunkUrl:        opts.SplunkUrl,
-    SplunkToken:      opts.SplunkToken,
-  }
+	// Splunkbot
+	sbot := &splunkbot.Splunkbot{
+		HttpClient:       client,
+		ListeningAddress: opts.ListeningAddress,
+		ListeningPort:    opts.ListeningPort,
+		SplunkSourcetype: opts.SplunkSourcetype,
+		SplunkIndex:      opts.SplunkIndex,
+		SplunkUrl:        opts.SplunkUrl,
+		SplunkToken:      opts.SplunkToken,
+	}
 
-  // Serving
-  err := sbot.Serve()
+	// Serving
+	err := sbot.Serve()
 
-  if err != nil {
-    log.Fatal(err)
-    os.Exit(1)
-  }
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
 }
--- splunkbot/splunkbot.go
+++ splunkbot/splunkbot.go
@@ -1,97 +1,97 @@
-// vim: set tabstop=2 expandtab autoindent smartindent:
+// vim: set tabstop=4 expandtab autoindent smartindent:
 
 package splunkbot
 
 import (
-  "fmt"
-  "bytes"
-  "io/ioutil"
-  "strings"
-  "net/url"
-  "net/http"
-  "encoding/json"
-  log "github.com/sirupsen/logrus"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
 )
 
 type SpunkHECMessage struct {
-  Time                string          `json:"time,omitempty"`
-  Host                string          `json:"host,omitempty"`
-  Source              string          `json:"source,omitempty"`
-  Sourcetype          string          `json:"sourcetype,omitempty"`
-  Index               string          `json:"index,omitempty"`
-  Event               interface{}     `json:"event"`
+	Time       string      `json:"time,omitempty"`
+	Host       string      `json:"host,omitempty"`
+	Source     string      `json:"source,omitempty"`
+	Sourcetype string      `json:"sourcetype,omitempty"`
+	Index      string      `json:"index,omitempty"`
+	Event      interface{} `json:"event"`
 }
 
 type Splunkbot struct {
-  HttpClient          *http.Client
-  ListeningAddress    string
-  ListeningPort       uint
-  SplunkSourcetype    string
-  SplunkIndex         string
-  SplunkUrl           string
-  SplunkToken         string
+	HttpClient       *http.Client
+	ListeningAddress string
+	ListeningPort    uint
+	SplunkSourcetype string
+	SplunkIndex      string
+	SplunkUrl        string
+	SplunkToken      string
 }
 
 func (sbot Splunkbot) Serve() error {
-  http.HandleFunc("/", sbot.alert)
-  err := http.ListenAndServe(fmt.Sprintf("%s:%d", sbot.ListeningAddress, sbot.ListeningPort), nil)
+	http.HandleFunc("/", sbot.alert)
+	err := http.ListenAndServe(fmt.Sprintf("%s:%d", sbot.ListeningAddress, sbot.ListeningPort), nil)
 
-  return err
+	return err
 }
 
 func (sbot Splunkbot) alert(w http.ResponseWriter, r *http.Request) {
-  log.Debugf("New request: %v", r)
+	log.Debugf("New request: %v", r)
 
-  var alert map[string]interface{}
-  var message SpunkHECMessage
+	var alert map[string]interface{}
+	var message SpunkHECMessage
 
-  // Decode input
-  buf, _  := ioutil.ReadAll(r.Body)
-  err     := json.Unmarshal(buf, &alert)
+	// Decode input
+	buf, _ := ioutil.ReadAll(r.Body)
+	err := json.Unmarshal(buf, &alert)
 
-  // if buf is not valid json we cast it as string
-  if err != nil {
-    message.Event = interface{}(string(buf))
-  } else {
-    message.Event = alert
-  }
+	// if buf is not valid json we cast it as string
+	if err != nil {
+		message.Event = interface{}(string(buf))
+	} else {
+		message.Event = alert
+	}
 
-  // Splunk Message 
-  message.Sourcetype  = sbot.SplunkSourcetype
-  message.Index       = sbot.SplunkIndex
+	// Splunk Message
+	message.Sourcetype = sbot.SplunkSourcetype
+	message.Index = sbot.SplunkIndex
 
-  if value, ok := alert["externalURL"]; ok {
-    u, _ := url.Parse(value.(string))
-    message.Host    = u.Hostname()
-    message.Source  = strings.TrimLeft(u.Path, "/")
-  }
+	if value, ok := alert["externalURL"]; ok {
+		u, _ := url.Parse(value.(string))
+		message.Host = u.Hostname()
+		message.Source = strings.TrimLeft(u.Path, "/")
+	}
 
-  // HTTP Splunk request
-  j, _  := json.Marshal(message)
-  jr    := bytes.NewReader(j)
+	// HTTP Splunk request
+	j, _ := json.Marshal(message)
+	jr := bytes.NewReader(j)
 
-  splunkReq, _ := http.NewRequest("POST", sbot.SplunkUrl, jr)
-  splunkReq.Header.Set("Authorization", "Splunk " + sbot.SplunkToken)
+	splunkReq, _ := http.NewRequest("POST", sbot.SplunkUrl, jr)
+	splunkReq.Header.Set("Authorization", "Splunk "+sbot.SplunkToken)
 
-  // Do request
-  resp, err := sbot.HttpClient.Do(splunkReq)
+	// Do request
+	resp, err := sbot.HttpClient.Do(splunkReq)
 
-  if err != nil {
-    log.Errorf("Failed to send request to splunk: %+v", err)
+	if err != nil {
+		log.Errorf("Failed to send request to splunk: %+v", err)
 
-    if resp != nil {
-      buf, _ := ioutil.ReadAll(resp.Body)
-      w.WriteHeader(resp.StatusCode)
-      w.Write(buf)
-    } else {
-      w.WriteHeader(503)
-      w.Write([]byte(fmt.Sprintf("Something went wrong:\n\n%+v\n", err)))
-    }
-  } else {
-    buf, _ := ioutil.ReadAll(resp.Body)
-    w.WriteHeader(resp.StatusCode)
-    w.Write(buf)
-  }
+		if resp != nil {
+			buf, _ := ioutil.ReadAll(resp.Body)
+			w.WriteHeader(resp.StatusCode)
+			w.Write(buf)
+		} else {
+			w.WriteHeader(503)
+			w.Write([]byte(fmt.Sprintf("Something went wrong:\n\n%+v\n", err)))
+		}
+	} else {
+		buf, _ := ioutil.ReadAll(resp.Body)
+		w.WriteHeader(resp.StatusCode)
+		w.Write(buf)
+	}
 
-  log.Debugf("End of request")
+	log.Debugf("End of request")
 }
